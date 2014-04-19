abd0a46cf73e9059cd992407805dbd58d9b8842d
Yonasu <yonasu@gmail.com>
2014-04-19 16:02:18 +0700

    mazeing

--- VidyaTutorial/VidyaTutorial/Maze.cs
+++ VidyaTutorial/VidyaTutorial/Maze.cs
@@ -15,10 +15,20 @@ namespace VidyaTutorial
         GraphicsDevice device;
         VertexBuffer floorBuffer;
         Color[] floorColors = new Color[2] { Color.White, Color.BlueViolet };
+
+        private Random rand = new Random();
+        public MazeCell[,] MazeCells = new MazeCell[mazeWidth, mazeHeight];
+
         public Maze(GraphicsDevice graphicsDevice)
         {
             this.device = graphicsDevice;
             BuildFloorBuffer();
+            for (int x = 0; x < mazeWidth; x++)
+                for (int z = 0; z < mazeHeight; z++)
+                {
+                    MazeCells[x, z] = new MazeCell();
+                }
+            GenerateMaze();
         }
 
         private void BuildFloorBuffer()
@@ -74,7 +84,7 @@ namespace VidyaTutorial
 
             return vList;
         }
-        
+
         public void Draw(Camera camera, BasicEffect effect)
         {
             effect.VertexColorEnabled = true;
@@ -91,6 +101,77 @@ namespace VidyaTutorial
                 floorBuffer.VertexCount / 3);
             }
         }
+        #region mazegeneration
+
+        public void GenerateMaze()
+        {
+            for (int x = 0; x < mazeWidth; x++)
+            {
+                for (int z = 0; z < mazeHeight; z++)
+                {
+                    MazeCells[x, z].Walls[0] = true;
+                    MazeCells[x, z].Walls[1] = true;
+                    MazeCells[x, z].Walls[2] = true;
+                    MazeCells[x, z].Walls[3] = true;
+                    MazeCells[x, z].Visited = false;
+                }
+                MazeCells[0, 0].Visited = true;
+                EvaluateCell(new Vector2(0, 0));
+            }
+        }
+
+        private void EvaluateCell(Vector2 cell)
+        {
+            List<int> neighborCells = new List<int>();
+            neighborCells.Add(0);
+            neighborCells.Add(1);
+            neighborCells.Add(2);
+            neighborCells.Add(3);
+            while (neighborCells.Count > 0)
+            {
+                int pick = rand.Next(0, neighborCells.Count);
+                int selectedNeighbor = neighborCells[pick];
+                neighborCells.RemoveAt(pick);
+                Vector2 neighbor = cell;
+                switch (selectedNeighbor)
+                {
+                    case 0: neighbor += new Vector2(0, -1);
+                        break;
+                    case 1: neighbor += new Vector2(1, 0);
+                        break;
+                    case 2: neighbor += new Vector2(0, 1);
+                        break;
+                    case 3: neighbor += new Vector2(-1, 0);
+                        break;
+                }
+                if (
+                (neighbor.X >= 0) &&
+                (neighbor.X < mazeWidth) &&
+                (neighbor.Y >= 0) &&
+                (neighbor.Y < mazeHeight)
+                )
+                {
+                    if (!MazeCells[(int)neighbor.X, (int)neighbor.Y].
+                    Visited)
+                    {
+                        MazeCells[
+                        (int)neighbor.X,
+                        (int)neighbor.Y].Visited = true;
+                        MazeCells[
+                        (int)cell.X,
+                        (int)cell.Y].Walls[selectedNeighbor] = false;
+                        MazeCells[
+                        (int)neighbor.X,
+                        (int)neighbor.Y].Walls[
+                        (selectedNeighbor + 2) % 4] = false;
+                        EvaluateCell(neighbor);
+                    }
+                }
+            }
+
+        }
+
+        #endregion
 
     }
 
